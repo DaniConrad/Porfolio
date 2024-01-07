8928aaf9f63c27e4eb7b812d1b03d2e3813cca1f
unknown <rubendanielconrad@gmail.com>
2024-01-07 19:53:46 -0300

    services v01 done

--- src/components/ServicesPage/ServicesPage.jsx
+++ src/components/ServicesPage/ServicesPage.jsx
@@ -3,22 +3,23 @@ import React from 'react'
 const ServicesPage = () => {
 
   const items = [
-    {name: "Gestión de redes sociales",  icon: "link"},
-    {name: "Asesorías",  icon: "easel-oultine"},
-    {name: "Moderación",  icon: "link"},
-    {name: "Optimización de biografía",  icon: "link"},
-    {name: "Presentación de resultados",  icon: "link"},
-    {name: "Estrategia de contenido",  icon: "link"},
+    {name: "Gestión de redes sociales",  icon: "./icons/manage.svg"},
+    {name: "Asesorías personalizadas",  icon: "./icons/advisory.svg"},
+    {name: "Moderación",  icon: "./icons/moderation.svg"},
+    {name: "Optimización de biografía",  icon: "./icons/biography.svg"},
+    {name: "Presentación de resultados",  icon: "./icons/results.svg"},
+    {name: "Estrategia de contenido",  icon: "./icons/strategy.svg"},
 
   ]
 
   return (
-    <div>
+    <div className='grid grid-cols-3 gap-4 p-10'>
       {
         items.map(item => (
-          <div  className="" key={Math.random()}>
-            <h2 className="cards-title" > {item.name} </h2>
-            <ion-icon name="easel-outline" color="light"/>
+          <div  className="flex flex-col items-center m-5 hover:scale-125 transition duration-500" key={Math.random()}>
+            <img src={item.icon} alt={item.name} className='w-2/4 m-1 fill-white '/>
+            <h2 className="text-xl text-white m-1 service_title"> {item.name} </h2>
+            
           </div>
         )
         )
