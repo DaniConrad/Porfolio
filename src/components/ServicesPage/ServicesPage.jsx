427ff0242d1e1f917407037328ab725a9e7270d5
unknown <rubendanielconrad@gmail.com>
2024-01-07 18:58:52 -0300

    packajes were updated, starting services page

--- src/components/ServicesPage/ServicesPage.jsx
+++ src/components/ServicesPage/ServicesPage.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 
 const ServicesPage = () => {
+
+  const items = [
+    {name: "Gestión de redes sociales",  icon: "link"},
+    {name: "Asesorías",  icon: "easel-oultine"},
+    {name: "Moderación",  icon: "link"},
+    {name: "Optimización de biografía",  icon: "link"},
+    {name: "Presentación de resultados",  icon: "link"},
+    {name: "Estrategia de contenido",  icon: "link"},
+
+  ]
+
   return (
-    <div>ServicesPage</div>
+    <div>
+      {
+        items.map(item => (
+          <div  className="" key={Math.random()}>
+            <h2 className="cards-title" > {item.name} </h2>
+            <ion-icon name="easel-outline" color="light"/>
+          </div>
+        )
+        )
+      }
+    </div>
   )
 }
 
