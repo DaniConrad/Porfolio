7bea82af2d7edb4dc393950d36770a6d24e609d4
unknown <rubendanielconrad@gmail.com>
2024-01-07 17:57:42 -0300

    added icons in footer

--- src/components/Footer/FooterMobile.jsx
+++ src/components/Footer/FooterMobile.jsx
@@ -5,18 +5,23 @@ const FooterMobile = () => {
   return (
     <div className='flex flex-wrap justify-center bg-black py-2'>
         <a href="google.com" className='mx-3'>
-           <ion-icon  size="large" name="logo-whatsapp"></ion-icon> 
+           <ion-icon  size="large" name="mail-outline" color="sdnd" />
         </a>
         <a href="https://www.instagram.com/soydanilanodaniela/" className='mx-3'>
-            <ion-icon size="large" name="logo-instagram"></ion-icon>
+            <ion-icon size="large" name="logo-whatsapp" color="sdnd" />
         </a>
         <a href="google.com" className='mx-3'>
-            <ion-icon size="large" name="mail-outline"></ion-icon>
+            <ion-icon size="large" name="logo-instagram" color="sdnd" />
         </a>
         <a href="google.com" className='mx-3'>
-            <ion-icon size="large" name="logo-behance"></ion-icon>
+            <ion-icon size="large" name="logo-tiktok" color="sdnd" />
         </a>
-        
+        <a href="google.com" className='mx-3'>
+            <ion-icon size="large" name="logo-linkedin" color="sdnd" />
+        </a>
+        <a href="google.com" className='mx-3'>
+            <ion-icon size="large" name="logo-behance" color="sdnd" />
+        </a>
         
     </div>
   )
