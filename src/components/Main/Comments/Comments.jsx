7e74632bc61d4a7a00366390960f869e340339f8
unknown <rubendanielconrad@gmail.com>
2024-01-10 20:21:07 -0300

    mobile version done

--- src/components/Main/Comments/Comments.jsx
+++ src/components/Main/Comments/Comments.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Comments = () => {
   return (
-    <div className="min-w-screen min-h-screen bg-gray-50 flex items-center justify-center">
+    <div className="min-w-screen sm:min-h-screen bg-gray-50 flex items-center justify-center">
       <div className="w-full bg-gray-200 border-t border-b border-gray-200 px-5 py-16 md:py-24 text-gray-800">
         <div className="w-full max-w-6xl mx-auto">
           <div className="text-center max-w-xl mx-auto">
